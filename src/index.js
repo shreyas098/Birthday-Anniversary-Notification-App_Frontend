5a8b43a2e2f2340aced23fa7db4717cabb8a0aba
Vishal Jain <vishal@kiprosh.com>
2021-04-26 12:14:50 +0530

    remove index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import MiniDrawer from "./components/MiniDrawer";
 
 ReactDOM.render(
   <React.StrictMode>
-    <MiniDrawer />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
