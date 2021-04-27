ae8501421a8d9e40cea0d7c1eb67b41f9a4e5b17
Imanpal Singh <49276301+imanpalsingh@users.noreply.github.com>
2021-04-27 13:07:13 +0530

    add render birthday cards

--- src/components/Wish/Wish.Component.jsx
+++ src/components/Wish/Wish.Component.jsx
@@ -1,6 +1,18 @@
+import BirthdayCard from "../BirthdayCard/BirthdayCard.component";
+import "./Wish.styles.css";
+
 const Wish = ()=>{
 
-    return <h1>Wish</h1>
+    return (
+        <div className="add-wishes">
+        <h1>Birthdays Today</h1>
+        <div className="birthday-cards">
+            <BirthdayCard />
+
+        </div>
+
+        </div>
+    )
 }
 
 export default Wish;
