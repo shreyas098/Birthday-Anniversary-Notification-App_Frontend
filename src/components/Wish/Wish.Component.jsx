596b501a03da909276d38c289deb89e0be56f523
Imanpal Singh <49276301+imanpalsingh@users.noreply.github.com>
2021-04-27 17:42:57 +0530

    render birthday card

--- src/components/Wish/Wish.Component.jsx
+++ src/components/Wish/Wish.Component.jsx
@@ -1,13 +1,44 @@
 import BirthdayCard from "../BirthdayCard/BirthdayCard.component";
 import "./Wish.styles.css";
+import Data from "../../DataService/Data";
+import {useEffect, useState} from "react";
 
-const Wish = ()=>{
 
+const Wish = (props)=>{
+
+    // For the associate data whose birthday is tommorrow
+    const [birthdayAssociates, updateBirthdayAssociates] = useState({});
+    
+    useEffect(()=>{
+        Data.getCurrentBirthdays(localStorage.getItem("token"))
+      .then(res=>{updateBirthdayAssociates(res)})
+      },[])
+    
+
+    
     return (
         <div className="add-wishes">
-        <h1>Birthdays Today</h1>
-        <div className="birthday-cards">
-            <BirthdayCard />
+        <h1>Birthdays Tommorow</h1>
+        <div className="birthday-cards" > 
+
+            {
+                birthdayAssociates.data?
+                birthdayAssociates.data.filter(({assoicateId})=>(
+                    assoicateId!==props.currentUserId
+                )).map(({assoicateId, associateName, dob})=>{
+                    
+                   
+                    dob = new Date().getFullYear() - Number(dob.split("T")[0].split("-")[0])
+
+                    return <BirthdayCard key={assoicateId} name={associateName} date={dob}/>
+                })
+                :
+               
+                <div className="no-birthday-found">
+                  <h2 className="error">Looks like no one was born on this date</h2>
+                </div>
+                
+            }
 
         </div>
 
