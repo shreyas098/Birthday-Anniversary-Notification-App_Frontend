65066207bce8821e617fa353cc0a7bed27051f88
Imanpal Singh <49276301+imanpalsingh@users.noreply.github.com>
2021-04-30 09:13:52 +0530

    ads loading state

--- src/components/Wish/Wish.Component.jsx
+++ src/components/Wish/Wish.Component.jsx
@@ -10,6 +10,7 @@ import {useEffect, useState} from "react";
 
 const Wish = (props)=>{
 
+    const [isLoading, updateIsLoading] = useState(true);
     // For the associate data whose birthday is tommorrow
     const [birthdayAssociates, updateBirthdayAssociates] = useState({});
 
@@ -26,6 +27,7 @@ const Wish = (props)=>{
             {
                 if(isMounted)
                 updateBirthdayAssociates(res.data)
+                updateIsLoading(false);
             }
         });
 
@@ -39,22 +41,24 @@ const Wish = (props)=>{
             <h1 className="title">Birthdays Tommorow</h1>
             <div className="birthday-cards" > 
                 {
+                    
                     /*
                         Get all the cards and filter out the current user's own card.
                     */
-                    birthdayAssociates.length?
-                    birthdayAssociates.filter(({assoicateId})=>(
-                        assoicateId!==props.currentUserId
-                    )).map(({assoicateId, associateName, dob, imageUrl})=>{
-                        
-                        // Calculate the age 
-                        dob = new Date().getFullYear() - Number(dob.split("T")[0].split("-")[0])
-                        return <BirthdayCard key={assoicateId} id={assoicateId} img = {imageUrl} name={associateName} date={dob}/>
-                    })
-                    :
-                    <div className="no-birthday-found">
-                    <h2 className="error">Looks like no one was born on this date</h2>
-                    </div>
+                    isLoading?"Loading ...":
+                        birthdayAssociates.length?
+                        birthdayAssociates.filter(({assoicateId})=>(
+                            assoicateId!==props.currentUserId
+                        )).map(({assoicateId, associateName, dob, imageUrl})=>{
+                            
+                            // Calculate the age 
+                            dob = new Date().getFullYear() - Number(dob.split("T")[0].split("-")[0])
+                            return <BirthdayCard key={assoicateId} id={assoicateId} img = {imageUrl} name={associateName} date={dob}/>
+                        })
+                        :
+                        <div className="no-birthday-found">
+                        <h2 className="error">Looks like no one was born on this date</h2>
+                        </div>
                     
                 }
             </div>
